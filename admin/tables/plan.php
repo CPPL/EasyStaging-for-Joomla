c11e026aaca5f3427adf43b6da23e5a4a5ae1192
Craig Phillips <craig@craigphillips.biz>
2011-11-22 11:28:55 +1000

    Add initial access xml, plan table & install script.
    Signed-off-by: Craig Phillips <craig@craigphillips.biz>

---
+++ admin/tables/plan.php
@@ -0,0 +1,23 @@
+<?php
+// No direct access
+defined('_JEXEC') or die('Restricted access');
+ 
+// import Joomla table library
+jimport('joomla.database.table');
+ 
+/**
+ * Hello Table class
+ */
+class EasyStagingTablePlan extends JTable
+{
+	/**
+	 * Constructor
+	 *
+	 * @param object Database connector object
+	 */
+	function __construct(&$db) 
+	{
+		parent::__construct('#__easystaging', 'id', $db);
+	}
+
+}

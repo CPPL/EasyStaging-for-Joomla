a1416787169f7f8b25d35392ffd419dd6789ab56
Craig Phillips <craig@craigphillips.biz>
2011-11-30 11:17:17 +1000

    Update install SQL to reflect table changes.
    Signed-off-by: Craig Phillips <craig@craigphillips.biz>

--- admin/sql/install.mysql.utf8.sql
+++ admin/sql/install.mysql.utf8.sql
@@ -1,61 +1,56 @@
-CREATE TABLE IF NOT EXISTS `#__easystaging` (
+CREATE TABLE IF NOT EXISTS `#__easystaging_plans` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) DEFAULT NULL,
-  `description` text,
-  `published` tinyint(1) DEFAULT NULL,
-  `checked_out` int(10) DEFAULT NULL,
-  `checked_out_time` datetime DEFAULT NULL,
-  `modified_by` int(10) DEFAULT NULL,
-  `created_by` int(10) DEFAULT NULL,
-  `modified` datetime DEFAULT NULL,
-  `created` datetime DEFAULT NULL,
-  `publish_up` datetime DEFAULT NULL,
-  `publish_down` datetime DEFAULT NULL,
-  `last_run` datetime DEFAULT NULL,
+  `name` varchar(255) NOT NULL DEFAULT '',
+  `description` text NOT NULL,
+  `published` tinyint(1) NOT NULL,
+  `checked_out` int(10) NOT NULL,
+  `checked_out_time` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `modified_by` int(10) NOT NULL,
+  `created_by` int(10) NOT NULL,
+  `modified` datetime NOT NULL,
+  `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `publish_up` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `publish_down` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `last_run` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
-CREATE TABLE IF NOT EXISTS `#__easystaging_schedules` (
+CREATE TABLE IF NOT EXISTS `#__easystaging_sites` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `push_plan` int(11) DEFAULT NULL,
-  `type` tinyint(1) DEFAULT NULL,
-  `next_start` datetime DEFAULT NULL,
-  `frequency_quanta` tinyint(1) DEFAULT NULL,
-  `frequency_value` int(11) DEFAULT NULL,
-  `last` datetime DEFAULT NULL,
-  `file_result` text,
-  `data_result` text,
+  `type` tinyint(1) NOT NULL,
+  `site_name` varchar(255) NOT NULL DEFAULT '',
+  `site_url` varchar(512) NOT NULL DEFAULT '',
+  `take_site_offline` bit(1) NOT NULL,
+  `site_path` varchar(1024) NOT NULL DEFAULT '',
+  `database_name` varchar(255) NOT NULL DEFAULT '',
+  `database_user` varchar(255) NOT NULL DEFAULT '',
+  `database_password` varchar(255) NOT NULL DEFAULT '',
+  `database_host` varchar(255) NOT NULL DEFAULT '',
+  `database_table_prefix` varchar(16) NOT NULL DEFAULT '',
+  `rsync_options` varchar(255) NOT NULL,
+  `file_exclusions` text NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
-CREATE TABLE IF NOT EXISTS `#__easystaging_target_sites` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `type` tinyint(1) DEFAULT NULL,
-  `site_name` varchar(255) DEFAULT NULL,
-  `site_url` varchar(512) DEFAULT NULL,
-  `take_site_offline` bit(1) DEFAULT NULL,
-  `site_path` varchar(1024) DEFAULT NULL,
-  `live_database_name` varchar(1024) DEFAULT NULL,
-  `live_database_user` varchar(1024) DEFAULT NULL,
-  `live_database_password` varchar(1024) DEFAULT NULL,
-  `live_database_host` varchar(255) DEFAULT NULL,
-  `live_database_table_prefix` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
-
-CREATE TABLE IF NOT EXISTS `#__easystaging_target_tables` (
+CREATE TABLE IF NOT EXISTS `#__easystaging_tables` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `targetsiteid` int(11) unsigned NOT NULL,
-  `targettablename` varchar(128) DEFAULT NULL,
-  `action` int(11) DEFAULT NULL,
-  `last` date DEFAULT NULL,
-  `lastresult` tinyint(1) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-CREATE TABLE IF NOT EXISTS `#__easystaging_targets` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `push_plan` int(11) DEFAULT NULL,
-  `target_site` int(11) DEFAULT NULL,
+  `siteid` int(11) unsigned NOT NULL,
+  `tablename` varchar(128) NOT NULL DEFAULT '',
+  `action` int(11) NOT NULL,
+  `last` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `lastresult` tinyint(1) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `#__easystaging_upshot` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `plan_id` int(11) unsigned NOT NULL,
+  `target_site_id` int(11) unsigned NOT NULL,
+  `rsync_cmd` text NOT NULL,
+  `rsync_exclusion` text NOT NULL,
+  `rsync_results` text NOT NULL,
+  `tables_to_copy` int(11) NOT NULL,
+  `tables_copied` int(11) NOT NULL,
+  `table_results` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COMMENT='Log of push results.';

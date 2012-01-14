7bae312c41e70ba89ebc41e0bafb6dddad70c6d4
Craig Phillips <craig@craigphillips.biz>
2012-01-14 15:55:48 +1000

    Update uninstall SQL file to match table names.
    Signed-off-by: Craig Phillips <craig@craigphillips.biz>

--- admin/sql/uninstall.mysql.utf8.sql
+++ admin/sql/uninstall.mysql.utf8.sql
@@ -1,9 +1,7 @@
 DROP TABLE IF EXISTS `#__easystaging_plans`;
 
-DROP TABLE IF EXISTS `#__easystaging_schedules`;
+DROP TABLE IF EXISTS `#__easystaging_sites`;
 
-DROP TABLE IF EXISTS `#__easystaging_target_sites`;
+DROP TABLE IF EXISTS `#__easystaging_tables`;
 
-DROP TABLE IF EXISTS `#__easystaging_target_tables`;
-
-DROP TABLE IF EXISTS `#__easystaging_targets`;
+DROP TABLE IF EXISTS `#__easystaging_upshot`;

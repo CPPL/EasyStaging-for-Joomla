4cd0de94cb5fd685edcdf8b00a6d6a23dba65686
Craig Phillips <craig@craigphillips.biz>
2012-01-17 23:51:08 +1000

    Add ACL checks to Plan tabs.
    Signed-off-by: Craig Phillips <craig@craigphillips.biz>

--- admin/views/plan/tmpl/edit.php
+++ admin/views/plan/tmpl/edit.php
@@ -6,6 +6,7 @@ JHtml::_('behavior.tooltip');
 JHtml::_('behavior.tooltip');
 JHtml::_('behavior.formvalidation');
 JHtml::_('behavior.keepalive');
+if(!$this->canDo->get('core.edit')) { return JError::raiseWarning(404, JText::_('JERROR_ALERTNOAUTHOR')); }
 ?>
 
 <form action="<?php echo JRoute::_('index.php?option=com_easystaging&layout=edit&id='.(int) $this->item->id); ?>"
@@ -62,11 +63,19 @@ JHtml::_('behavior.keepalive');
 	<div class="clr"></div>
 <!-- Tab UI -->
 	<div id="com_easystaging_plan_tabs" class="width-100">
-	<?php echo JHtml::_('tabs.start', 'com_easystaging_tabs', array('useCookie'=>true));?> 
-		<?php echo JHtml::_('tabs.panel', JText::_('COM_EASYSTAGING_STATUS_TAB'), 'status'); ?><?php echo $this->loadTemplate('status');?>
-		<?php echo JHtml::_('tabs.panel', JText::_('COM_EASYSTAGING_LOCALSITE_TAB'), 'local_site'); ?><?php echo $this->loadTemplate('local');?>
-		<?php echo JHtml::_('tabs.panel', JText::_('COM_EASYSTAGING_REMOTESITE_TAB'), 'remote_site'); ?><?php echo $this->loadTemplate('remote');?>
-		<?php echo JHtml::_('tabs.panel', JText::_('COM_EASYSTAGING_TABLESETTINGS_TAB'), 'table_settings'); ?><?php echo $this->loadTemplate('tables');?>
+	<?php echo JHtml::_('tabs.start', 'com_easystaging_tabs', array('useCookie'=>false));?> 
+		<?php if($this->canDo->get('easystaging.run')) {
+			echo JHtml::_('tabs.panel', JText::_('COM_EASYSTAGING_STATUS_TAB'), 'status');
+			echo $this->loadTemplate('status');
+		} ?>
+		<?php if($this->canDo->get('core.edit') || ($this->canDo->get('core.create') && ($this->item->id == 0))) {
+			echo JHtml::_('tabs.panel', JText::_('COM_EASYSTAGING_LOCALSITE_TAB'), 'local_site');
+			echo $this->loadTemplate('local');
+			echo JHtml::_('tabs.panel', JText::_('COM_EASYSTAGING_REMOTESITE_TAB'), 'remote_site');
+			echo $this->loadTemplate('remote');
+			echo JHtml::_('tabs.panel', JText::_('COM_EASYSTAGING_TABLESETTINGS_TAB'), 'table_settings');
+			echo $this->loadTemplate('tables');
+		} ?>
 	<?php echo JHtml::_('tabs.end');?> 
 	</div>
 	<div class="clr"></div>

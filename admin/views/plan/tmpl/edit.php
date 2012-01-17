e0c7ad9ce349dd821caa36b6df60ff7d1ff5a2f9
Craig Phillips <craig@craigphillips.biz>
2012-01-17 23:51:59 +1000

    Move Plan detail into a sub-layout
    Signed-off-by: Craig Phillips <craig@craigphillips.biz>

--- admin/views/plan/tmpl/edit.php
+++ admin/views/plan/tmpl/edit.php
@@ -1,7 +1,6 @@
 <?php
 // No direct access
 defined('_JEXEC') or die('Restricted access');
-JHtml::_('behavior.tooltip');
 // Load the tooltip behavior.
 JHtml::_('behavior.tooltip');
 JHtml::_('behavior.formvalidation');
@@ -11,56 +10,7 @@ if(!$this->canDo->get('core.edit')) { return JError::raiseWarning(404, JText::_(
 
 <form action="<?php echo JRoute::_('index.php?option=com_easystaging&layout=edit&id='.(int) $this->item->id); ?>"
       method="post" name="adminForm" id="easystaging-form">
-<!-- Main Form Body -->
-	<div class="width-60 fltlft">
-		<fieldset class="adminform">
-			<legend><?php echo JText::_( 'COM_EASYSTAGING_PLAN_DETAILS' ); ?></legend>
-			<ul class="adminformlist">
-				<li><?php echo $this->form->getLabel('name'); ?> <?php echo $this->form->getInput('name'); ?></li>
-				<li><?php echo $this->form->getLabel('description'); ?> <?php echo $this->form->getInput('description'); ?></li>
-				<li><?php echo $this->form->getLabel('published'); ?> <?php echo $this->form->getInput('published'); ?></li>
-				<li><?php echo $this->form->getLabel('id'); ?> <?php echo $this->form->getInput('id'); ?></li>
-
-				<li><?php echo $this->form->getLabel('access'); ?>
-				<?php echo $this->form->getInput('access'); ?></li>
-
-			<?php if ($this->canDo->get('core.admin')): ?>
-				<li><span class="faux-label"><?php echo JText::_('JGLOBAL_ACTION_PERMISSIONS_LABEL'); ?></span>
-					<div class="button2-left"><div class="blank">
-						<button type="button" onclick="document.location.href='#access-rules';">
-							<?php echo JText::_('JGLOBAL_PERMISSIONS_ANCHOR'); ?>
-						</button>
-					</div></div>
-				</li>
-			<?php endif; ?>
-			</ul>
-		</fieldset>
-	</div>
-
-<!-- Parameter Sidebar -->
-	<div class="width-40 fltrt">
-		<?php echo JHtml::_('sliders.start','content-sliders-'.$this->item->id, array('useCookie'=>1)); ?>
-		<?php echo JHtml::_('sliders.panel',JText::_('COM_EASYSTAGING_BASIC_ATTRIBUTES_LABEL'), 'basic-options'); ?>
-			<fieldset class="panelform">
-				<ul class="adminformlist">
-					<li><?php echo $this->form->getLabel('created_by'); ?> <?php echo $this->form->getInput('created_by'); ?></li>
-	
-					<li><?php echo $this->form->getLabel('created'); ?> <?php echo $this->form->getInput('created'); ?></li>
-	
-					<li><?php echo $this->form->getLabel('publish_up'); ?> <?php echo $this->form->getInput('publish_up'); ?></li>
-	
-					<li><?php echo $this->form->getLabel('publish_down'); ?> <?php echo $this->form->getInput('publish_down'); ?></li>
-	
-					<?php if ($this->item->modified_by) : ?>
-						<li><?php echo $this->form->getLabel('modified_by'); ?> <?php echo $this->form->getInput('modified_by'); ?></li>
-						<li><?php echo $this->form->getLabel('modified'); ?> <?php echo $this->form->getInput('modified'); ?></li>
-					<?php endif; ?>
-				</ul>
-			</fieldset>
-		<?php echo JHtml::_('sliders.end'); ?>
-	</div>
-
-	<div class="clr"></div>
+	<?php echo $this->loadTemplate('plan');?>
 <!-- Tab UI -->
 	<div id="com_easystaging_plan_tabs" class="width-100">
 	<?php echo JHtml::_('tabs.start', 'com_easystaging_tabs', array('useCookie'=>false));?> 

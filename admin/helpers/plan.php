2acd7d4bf0e545b0641ebbcc9c05a4e82d3654d1
Craig Phillips <craig@craigphillips.biz>
2012-01-22 01:58:11 +1000

    Add functions to create a zip archive from files & directories.
    Add a remove directory function as well.
    
    Signed-off-by: Craig Phillips <craig@craigphillips.biz>

--- admin/helpers/plan.php
+++ admin/helpers/plan.php
@@ -100,5 +100,90 @@ class PlanHelper
 		}
 	}
 	
+	/*
+	 * Tools for zipping directories & files.
+	 */
+	public static function createZip($files = array(), $destination = '', $stripPath = '', $overwrite = false)
+	{
+		//if the zip file already exists and overwrite is false, return false
+		if(file_exists($destination) && !$overwrite) {
+			return false;
+		}
+		//vars
+		$valid_files = array();
+		//if files were passed in...
+		if(is_array($files)) {
+			//cycle through each file
+			foreach($files as $file) {
+				//make sure the file exists
+				if(file_exists($file)) {
+					$valid_files[] = $file;
+				}
+			}
+		}
+		//if we have good files...
+		if(count($valid_files)) {
+			if(class_exists('ZipArchive')) { // Wouldn't you know it, some servers don't have zip.
+				//create the archive
+				$zip = new ZipArchive();
+				if($zip->open($destination,$overwrite ? ZIPARCHIVE::OVERWRITE : ZIPARCHIVE::CREATE) !== true) {
+					return false;
+				}
+				//add the files
+				foreach($valid_files as $file) {
+					$pathFoundInFile = strpos($file, $stripPath);
+					if(($pathFoundInFile != false) || ($pathFoundInFile == 0)) $pathFoundInFile = true;
+					if(($stripPath == '') || !$pathFoundInFile) {
+						$localName = $file;
+					} else {
+						$localName = str_replace($stripPath, '', $file);
+					}
+					$zip->addFile($file, $localName);
+				}
+				//debug
+				//echo 'The zip archive contains ',$zip->numFiles,' files with a status of ',$zip->status;
+			  
+				//close the zip -- done!
+				$zip->close();
+			  
+				//check to make sure the file exists
+				return file_exists($destination);
+			}
+		}
+
+		return false;
+	}
 	
+	public static function directoryToArray($directory, $recursive = false)
+	{
+		$array_items = array();
+		if ($handle = opendir($directory)) {
+			while (false !== ($file = readdir($handle))) {
+				if (($file != '.') && ($file != '..') && ($file != "Thumbs.db") && ($file != '.DS_Store')) {
+					if (is_dir($directory . '/' . $file)) {
+						if($recursive) {
+							$array_items = array_merge($array_items, directoryToArray($directory. '/' . $file, $recursive));
+						}
+						$file = $directory . '/' . $file;
+						$array_items[] = preg_replace('/\/\//si', '/', $file);
+					} else {
+						$file = $directory . '/' . $file;
+						$array_items[] = preg_replace('/\/\//si', '/', $file);
+					}
+				}
+			}
+			closedir($handle);
+		}
+		return $array_items;
+	}
+
+	function remove_this_directory($directory, $recursive = false) {
+		foreach(scandir($directory) as $file) {
+			if ('.' === $file || '..' === $file) continue;
+			if (is_dir("$directory/$file")) remove_this_directory("$directory/$file");
+			else unlink("$directory/$file");
+		}
+		rmdir($directory);
+	}
 }
+

1e217f9aeba043ba3d8c4ef846f2a9038e31f965
Craig Phillips <craig@craigphillips.biz>
2013-04-09 21:08:47 +1000

    Apply coding standards to plan helper.
    Fix undefined method 'remove_this_directory'
    
    Signed-off-by: Craig Phillips <craig@craigphillips.biz>

--- admin/helpers/plan.php
+++ admin/helpers/plan.php
@@ -6,44 +6,59 @@ defined('_JEXEC') or die;
 /**
  * EasyStaging component helper.
  *
+ * @package  EasyStaging
+ *
+ * @since    1.0.0
+ *
  */
 class PlanHelper
 {
 	public static $extension = 'com_easystaging';
+
 	public static $base_assett = 'plan';
-	private static $ext_actions = array('easystaging.run');
+
+	private static $_ext_actions = array('easystaging.run');
 
 	/**
-	 * Gets the local site record for the plan.
+	 * Prefix method to Get the local site record for the plan.
 	 *
+	 * @param   int  $plan_id  Plan identity
+	 *
+	 * @return  object
 	 */
 	public static function getLocalSite($plan_id = 0)
 	{
-		return self::_loadLocalSiteRecord($plan_id);
-	}
-	
-	public static function getRemoteSite($plan_id = 0)
-	{
-		return self::_loadRemoteSiteRecord($plan_id);
+		return self::_loadSiteRecord($plan_id, 1);
 	}
 
-	private static function _loadLocalSiteRecord($plan_id)
+	/**
+	 * Prefix method to Get the remote site record for the plan.
+	 *
+	 * @param   int  $plan_id  Plan identity
+	 *
+	 * @return  object
+	 */
+	public static function getRemoteSite($plan_id = 0)
 	{
-		$type = 1; // Local site
-		return self::_loadSiteRecord($plan_id, $type);
+		return self::_loadSiteRecord($plan_id, 2);
 	}
-	private static function _loadRemoteSiteRecord($plan_id)
-	{
-		$type = 2; // Live/Target site
-		return self::_loadSiteRecord($plan_id, $type);
-	}
+
+	/**
+	 * Gets a site record based on the plan_id and the site type
+	 *
+	 * @param   int  $plan_id  Plan identity
+	 *
+	 * @param   int  $type     Local (1) or Remote (2) site.
+	 *
+	 * @return  object
+	 */
 	private static function _loadSiteRecord($plan_id, $type)
 	{
 		// Load our site record
-		JTable::addIncludePath(JPATH_ADMINISTRATOR.'/components/com_easystaging/tables');
+		JTable::addIncludePath(JPATH_ADMINISTRATOR . '/components/com_easystaging/tables');
 		$Sites = JTable::getInstance('Site', 'EasyStagingTable');
-	
-		if ($Sites->load(array('plan_id'=>$plan_id, 'type'=>$type)))
+
+		if ($Sites->load(array('plan_id' => $plan_id, 'type' => $type)))
 		{
 			return $Sites;
 		}
@@ -56,35 +71,46 @@ class PlanHelper
 	/**
 	* Gets a list of the actions that can be performed.
 	*
-	* @param	int		The Plan ID.
+	* @param   int  $id  The Plan ID.
 	*
-	* @return	JObject
+	* @return  JObject
 	*/
 	public static function getActions($id = 0)
 	{
 		$user	= JFactory::getUser();
 		$result	= new JObject;
-	
+
 		if (empty($id))
 		{
 			$assetName = self::$extension;
 		}
-		else {
+		else
+		{
 			$assetName = self::$extension . '.' . self::$base_assett . '.' . (int) $id;
 		}
-	
-		$actions = array_merge( array( 'core.admin', 'core.manage', 'core.create', 'core.edit', 'core.edit.own', 'core.edit.state', 'core.delete' ),
-								self::$ext_actions );
-	
+
+		$actions = array_merge(
+			array( 'core.admin', 'core.manage', 'core.create', 'core.edit', 'core.edit.own', 'core.edit.state', 'core.delete' ),
+			self::$_ext_actions
+		);
+
 		foreach ($actions as $action)
 		{
 			$result->set($action,	$user->authorise($action, $assetName));
 		}
-	
+
 		return $result;
 	}
 
-	public  static function loadJSLanguageKeys($jsFile) {
+	/**
+	 * Parses a javascript file looking for JText keys and then loads them ready for use.
+	 *
+	 * @param   string  $jsFile  Path to the javascript file.
+	 *
+	 * @return bool
+	 */
+	public  static function loadJSLanguageKeys($jsFile)
+	{
 		if (isset($jsFile))
 		{
 			$jsFile = JPATH_SITE . $jsFile;
@@ -93,60 +119,82 @@ class PlanHelper
 		{
 			return false;
 		}
-	
+
 		if ($jsContents = file_get_contents($jsFile))
 		{
 			$languageKeys = array();
 			preg_match_all('/Joomla\.JText\._\(\'(.*?)\'\)\)?/', $jsContents, $languageKeys);
 			$languageKeys = $languageKeys[1];
+
 			foreach ($languageKeys as $lkey)
 			{
 				JText::script($lkey);
 			}
 		}
 	}
-	
-	/*
+
+	/**
 	 * Tools for zipping directories & files.
+	 */
+
+
+	/**
+	 * Create a Zip file
+	 *
+	 * @param   array   $files        Files to include in zip
+	 *
+	 * @param   string  $destination  Zip file to put files into.
+	 *
+	 * @param   string  $stripPath    Base path elements to be stripped from zip's local structure
+	 *
+	 * @param   bool    $overwrite    Flag to allow overwritting of an existing zip of the same name
+	 *
+	 * @return  bool
 	 */
 	public static function createZip($files = array(), $destination = '', $stripPath = '', $overwrite = false)
 	{
-		//if the zip file already exists and overwrite is false, return false
+		// If the zip file already exists and overwrite is false, return false
 		if (file_exists($destination) && !$overwrite)
 		{
 			return false;
 		}
-		//vars
+
+		// Vars
 		$valid_files = array();
-		//if files were passed in...
+
+		// If files were passed in...
 		if (is_array($files))
 		{
-			//cycle through each file
+			// Cycle through each file
 			foreach ($files as $file)
 			{
-				//make sure the file exists
+				// Make sure the file exists
 				if (file_exists($file))
 				{
 					$valid_files[] = $file;
 				}
 			}
 		}
-		//if we have good files...
+
+		// If we have good files...
 		if (count($valid_files))
 		{
 			// Wouldn't you know it, some servers don't have zip.
 			if (class_exists('ZipArchive'))
 			{
-				//create the archive
-				$zip = new ZipArchive();
-				if ($zip->open($destination,$overwrite ? ZIPARCHIVE::OVERWRITE : ZIPARCHIVE::CREATE) !== true)
+				// Create the archive
+				$zip = new ZipArchive;
+
+				if ($zip->open($destination, $overwrite ? ZIPARCHIVE::OVERWRITE : ZIPARCHIVE::CREATE) !== true)
 				{
 					return false;
 				}
-				//add the files
+
+				// Add the files
 				foreach ($valid_files as $file)
 				{
 					$pathFoundInFile = strpos($file, $stripPath);
+
 					if (($pathFoundInFile != false) || ($pathFoundInFile == 0))
 					{
 						$pathFoundInFile = true;
@@ -161,23 +209,34 @@ class PlanHelper
 					}
 					$zip->addFile($file, $localName);
 				}
-				//debug
-				//echo 'The zip archive contains ',$zip->numFiles,' files with a status of ',$zip->status;
-			  
-				//close the zip -- done!
+
+				// Debug
+				// echo 'The zip archive contains ',$zip->numFiles,' files with a status of ',$zip->status;
+
+				// Close the zip -- done!
 				$zip->close();
-			  
-				//check to make sure the file exists
+
+				// Check to make sure the file exists
 				return file_exists($destination);
 			}
 		}
 
 		return false;
 	}
-	
+
+	/**
+	 * Convert a directory path to an array of files.
+	 *
+	 * @param   string  $directory  The path to the directory.
+	 *
+	 * @param   bool    $recursive  If true, the path is searched recursively within each sub-directory.
+	 *
+	 * @return array
+	 */
 	public static function directoryToArray($directory, $recursive = false)
 	{
 		$array_items = array();
+
 		if ($handle = opendir($directory))
 		{
 			while (false !== ($file = readdir($handle)))
@@ -188,7 +247,7 @@ class PlanHelper
 					{
 						if ($recursive)
 						{
-							$array_items = array_merge($array_items, directoryToArray($directory. '/' . $file, $recursive));
+							$array_items = array_merge($array_items, directoryToArray($directory . '/' . $file, $recursive));
 						}
 						$file = $directory . '/' . $file;
 						$array_items[] = preg_replace('/\/\//si', '/', $file);
@@ -205,7 +264,15 @@ class PlanHelper
 		return $array_items;
 	}
 
-	public static function remove_this_directory($directory, $recursive = false) {
+	/**
+	 * Deletes the directory at the supplied path.
+	 *
+	 * @param   string  $directory  The path of the directory to be deleted.
+	 *
+	 * @return  null
+	 */
+	public static function remove_this_directory($directory)
+	{
 		foreach (scandir($directory) as $file)
 		{
 			if ('.' === $file || '..' === $file)
@@ -214,7 +281,7 @@ class PlanHelper
 			}
 			if (is_dir("$directory/$file"))
 			{
-				remove_this_directory("$directory/$file");
+				self::remove_this_directory("$directory/$file");
 			}
 			else
 			{

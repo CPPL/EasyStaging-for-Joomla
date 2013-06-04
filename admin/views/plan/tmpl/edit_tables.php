69496f19a54bf689627f725708bb077c51b87a2f
Craig Phillips <craig@craigphillips.biz>
2013-06-04 15:29:00 +1000

    Change filter label to the right global JText token.
    Signed-off-by: Craig Phillips <craig@craigphillips.biz>

--- admin/views/plan/tmpl/edit_tables.php
+++ admin/views/plan/tmpl/edit_tables.php
@@ -3,7 +3,7 @@
 defined('_JEXEC') or die('Restricted Access'); ?>
 	<div>
 		<p class="tab-description"><?php echo JText::_('COM_EASYSTAGING_TABLESETTINGS_TAB_DESC')?>
-			<span id="table-filters" title="<?php echo JText::_('COM_EASYSTAGING_JS_TABLE_FILTERS'); ?>"><?php echo JText::_('JGLOBAL_FILTER_TYPE_LABEL'); ?>:&nbsp;
+			<span id="table-filters" title="<?php echo JText::_('COM_EASYSTAGING_JS_TABLE_FILTERS'); ?>"><?php echo JText::_('JSEARCH_FILTER_LABEL'); ?>&nbsp;
 				<span id="allTablesFilter" class="hasTip" title="<?php echo JText::_('COM_EASYSTAGING_JS_ALL_TABLES_FILTER'); ?>"><button id="allTables" type="button" class="startBtns" ><?php echo JText::_('COM_EASYSTAGING_JS_ALL_TABLES_BTN'); ?></button></span>
 				<span id="skippedTablesFilter" class="hasTip" title="<?php echo JText::_('COM_EASYSTAGING_JS_SKIP_TABLES_FILTER'); ?>"><button id="allTables" type="button" class="startBtns" ><?php echo JText::_('COM_EASYSTAGING_JS_SKIP_TABLES_BTN'); ?></button></span>
 				<span id="pushTablesFilter" class="hasTip" title="<?php echo JText::_('COM_EASYSTAGING_JS_PUSH_TABLES_FILTER'); ?>"><button id="allTables" type="button" class="startBtns" ><?php echo JText::_('COM_EASYSTAGING_JS_PUSH_TABLES_BTN'); ?></button></span>

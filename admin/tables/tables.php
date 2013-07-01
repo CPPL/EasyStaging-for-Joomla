e79b8fc6d53e5bda5eaf9b8bc1bd044d2d5b8b86
Craig Phillips <craig@craigphillips.biz>
2013-07-01 13:35:51 +1000

    Add a check() method to the tables table so that merge actions only apply to tables with a Primary Key.
    Signed-off-by: Craig Phillips <craig@craigphillips.biz>

--- admin/tables/tables.php
+++ admin/tables/tables.php
@@ -4,7 +4,17 @@ defined('_JEXEC') or die('Restricted access');
  
 // import Joomla table library
 jimport('joomla.database.table');
- 
+
+// Load our run helper
+if (file_exists(JPATH_COMPONENT_ADMINISTRATOR . '/helpers/run.php'))
+{
+	require_once JPATH_COMPONENT_ADMINISTRATOR . '/helpers/run.php';
+}
+else
+{
+	die("EasyStaging isn't installed correctly.");
+}
+
 /**
  * Hello Table class
  */
@@ -13,10 +23,54 @@ class EasyStagingTableTables extends JTable
 	/**
 	 * Constructor
 	 *
-	 * @param object Database connector object
+	 * @param   JDatabase  &$db  DB connector object
 	 */
-	function __construct(&$db) 
+	public function __construct(&$db)
 	{
 		parent::__construct('#__easystaging_tables', 'id', $db);
+	}
+
+	/**
+	 * Method to perform sanity checks on the JTable instance properties to ensure
+	 * they are safe to store in the database.  Child classes should override this
+	 * method to make sure the data they are storing in the database is safe and
+	 * as expected before storage.
+	 *
+	 * @return  boolean  True if the instance is sane and able to be stored in the database.
+	 *
+	 * @link    http://docs.joomla.org/JTable/check
+	 * @since   11.1
+	 */
+	public function check()
+	{
+		// Assume success, i.e. most tables have an index
+		$checkResult = true;
+
+		$config = JFactory::getConfig();
+		$dbName = $config->get('db');
+		$db = $this->getDbo();
+
+		// Make sure the action is compatible with the table, e.g. merges require a PK and not all tables have an index.
+		// Now we need the primary key for the table...
+		$table = $this->tablename;
+		$action = $this->action;
+
+		// Range doesn't work with CONST - seriously... WTF? (Note these are raw actions from the UI)
+		$mergeActions = range(3, 5);
+
+		if (in_array($action, $mergeActions))
+		{
+			$pkSQL = RunHelper::buildTablePkQuery($dbName, $table);
+			$db->setQuery($pkSQL);
+			$tableProfile = $db->loadAssoc();
+
+			if ($tableProfile == null)
+			{
+				$this->action = 0;
+				JFactory::getApplication()->enqueueMessage(JText::sprintf('COM_EASYSTAGING_TABLE_MERGE_ACTION_NOT_VALID_FOR_X', $table), 'Notice');
+			}
+		}
+
+		return $checkResult;
 	}
 }

f1b95879dfc6c5645d03316fadd00085b4ddd4a1
Craig Phillips <craig@craigphillips.biz>
2013-07-01 13:22:04 +1000

    Add addMessages() to allow additional messages to be displayed without clearing existing.
    Signed-off-by: Craig Phillips <craig@craigphillips.biz>

--- media/js/atools.js
+++ media/js/atools.js
@@ -295,6 +295,37 @@ cppl_tools.isNumber = function (n) {
 	return !isNaN(parseFloat(n)) && isFinite(n);
 }
 
+cppl_tools.addMessages = function(messages) {
+    var container = document.id('system-message-container');
+
+    var dl = new Element('dl', {
+        id: 'system-message',
+        role: 'alert'
+    });
+    Object.each(messages, function (item, type) {
+        var dt = new Element('dt', {
+            'class': type,
+            html: type
+        });
+        dt.inject(dl);
+        var dd = new Element('dd', {
+            'class': type
+        });
+        dd.addClass('message');
+        var list = new Element('ul');
+
+        Array.each(item, function (item, index, object) {
+            var li = new Element('li', {
+                html: item
+            });
+            li.inject(list);
+        }, this);
+        list.inject(dd);
+        dd.inject(dl);
+    }, this);
+    dl.inject(container);
+};
+
 /**
 sprintf() for JavaScript 0.7-beta1
 http://www.diveintojavascript.com/projects/javascript-sprintf

648c2a0af198d0323476fe64928e1b73fabff644
Craig Phillips <craig@craigphillips.biz>
2014-05-11 21:27:57 +1000

    Fix JSHint issues.
    Signed-off-by: Craig Phillips <craig@craigphillips.biz>

--- media/js/plans.js
+++ media/js/plans.js
@@ -32,15 +32,18 @@ window.addEvent('domready', function(){
 });
 
 Joomla.submitbutton = function(pressbutton) {
-	if (pressbutton == 'plans.delete')
+    "use strict";
+	if (pressbutton === 'plans.delete')
 	{
-		 deleteOK = confirm(Joomla.JText._('COM_EASYSTAGING_PLAN_JS_CONFIRM_DELETE'));
+		 var deleteOK = confirm(Joomla.JText._('COM_EASYSTAGING_PLAN_JS_CONFIRM_DELETE'));
 		 if (deleteOK)
 		 {
 			 Joomla.submitform(pressbutton);
 		 }
 		 else
-			 return 0;
+         {
+             return 0;
+         }
 	}
 	Joomla.submitform(pressbutton);
-}
+};

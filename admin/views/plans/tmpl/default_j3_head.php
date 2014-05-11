9ae4ea65ae231960bd2b96f1430a8c7e5ac760ab
Craig Phillips <craig@craigphillips.biz>
2014-05-11 17:26:14 +1000

    Add J3 Templates
    Signed-off-by: Craig Phillips <craig@craigphillips.biz>

---
+++ admin/views/plans/tmpl/default_j3_head.php
@@ -0,0 +1,18 @@
+<?php defined('_JEXEC') or die('Restricted access'); ?>
+<tr>
+	<th width="20">
+		<input type="checkbox" name="toggle" value="" onclick="checkAll(<?php echo count( $this->items ); ?>);" />
+	</th>
+	<th width="40%">
+		<?php echo JText::_( 'COM_EASYSTAGING_PLAN' ); ?>
+	</th>
+	<th>
+		<?php echo JText::_( 'COM_EASYSTAGING_DESCRIPTION' ); ?>
+	</th>
+	<th width="5%" class="center">
+		<?php echo JText::_( 'COM_EASYSTAGING_PUBLISHED' ); ?>
+	</th>
+	<th width="5%">
+		<?php echo JText::_( 'JGRID_HEADING_ID' ); ?>
+	</th>
+</tr>            

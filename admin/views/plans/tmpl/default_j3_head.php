6275edd285925b79c9a1f4a180e319e2bdb9722f
Craig Phillips <craig@craigphillips.biz>
2014-05-11 18:12:44 +1000

    Fix column widths etc.
    Signed-off-by: Craig Phillips <craig@craigphillips.biz>

--- admin/views/plans/tmpl/default_j3_head.php
+++ admin/views/plans/tmpl/default_j3_head.php
@@ -1,9 +1,9 @@
 <?php defined('_JEXEC') or die('Restricted access'); ?>
 <tr>
-	<th width="20">
+	<th width="20" class="center">
 		<input type="checkbox" name="toggle" value="" onclick="checkAll(<?php echo count( $this->items ); ?>);" />
 	</th>
-	<th width="40%">
+	<th width="25%">
 		<?php echo JText::_( 'COM_EASYSTAGING_PLAN' ); ?>
 	</th>
 	<th>
@@ -12,7 +12,7 @@
 	<th width="5%" class="center">
 		<?php echo JText::_( 'COM_EASYSTAGING_PUBLISHED' ); ?>
 	</th>
-	<th width="5%">
+	<th width="5%" class="center">
 		<?php echo JText::_( 'JGRID_HEADING_ID' ); ?>
 	</th>
 </tr>            

d82651e3cfb1f42a5addf1cbfebbeb6daa9dcb97
Craig Phillips <craig@craigphillips.biz>
2014-05-25 23:59:32 +1000

    Fix formatting of Plans list on mobile devices.
    Signed-off-by: Craig Phillips <craig@craigphillips.biz>

--- admin/views/plans/tmpl/default_j3_foot.php
+++ admin/views/plans/tmpl/default_j3_foot.php
@@ -2,6 +2,4 @@
 // No direct access to this file
 defined('_JEXEC') or die('Restricted Access');
 ?>
-<tr>
-	<td colspan="4"><?php echo $this->pagination->getListFooter(); ?></td>
-</tr>
+<caption><?php echo $this->pagination->getListFooter(); ?></caption>
--- admin/views/plans/tmpl/default_j3_head.php
+++ admin/views/plans/tmpl/default_j3_head.php
@@ -1,18 +1,18 @@
 <?php defined('_JEXEC') or die('Restricted access'); ?>
 <tr>
-	<th width="20" class="center">
+	<th width="1%" class="center">
 		<input type="checkbox" name="toggle" value="" onclick="checkAll(<?php echo count( $this->items ); ?>);" />
 	</th>
-	<th width="25%">
+	<th width="25%" >
 		<?php echo JText::_( 'COM_EASYSTAGING_PLAN' ); ?>
 	</th>
-	<th>
+	<th class="hidden-phone">
 		<?php echo JText::_( 'COM_EASYSTAGING_DESCRIPTION' ); ?>
 	</th>
-	<th width="5%" class="center">
+	<th width="1%" class="center">
 		<?php echo JText::_( 'COM_EASYSTAGING_PUBLISHED' ); ?>
 	</th>
-	<th width="5%" class="center">
+	<th width="1%" class="center">
 		<?php echo JText::_( 'JGRID_HEADING_ID' ); ?>
 	</th>
 </tr>            

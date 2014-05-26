a63d34292b3305c7ee525eb7d6080628e69d3b08
Craig Phillips <craig@craigphillips.biz>
2014-05-26 17:55:42 +1000

    Fix silent failure of Rsync Test button when plan is unpublished.
    Signed-off-by: Craig Phillips <craig@craigphillips.biz>

--- media/js/plan.js
+++ media/js/plan.js
@@ -320,8 +320,9 @@ com_EasyStaging.setUp  = function (isPublished)
     this.tableFilter           = null;
     this.totalTables           = 0;
     this.tablesHidden          = 0;
+    this.isPublished           = isPublished;
 
-	if (cppl_tools.getID() === 0 && isPublished)
+	if (cppl_tools.getID() === "0" && isPublished)
 	{
         this.lockOutBtns(false);
     }
@@ -748,12 +749,20 @@ com_EasyStaging.testResult = function( response )
 com_EasyStaging.checkRsyncWorks = function()
 {
     "use strict";
-    // Add the "test started" message
-    this.hilightStatusMessages();
-    this.appendTextToCurrentStatus(Joomla.JText._('COM_EASYSTAGING_JSON_TEST_RSYNC_STARTED'));
+    if(this.isPublished)
+    {
+        // Add the "test started" message
+        this.hilightStatusMessages();
+        this.appendTextToCurrentStatus(Joomla.JText._('COM_EASYSTAGING_JSON_TEST_RSYNC_STARTED'));
 
-    this.requestData.es_drfca = 1;
-    this.start('startFile', true);
-    this.requestData.es_drfca = 0;
+        this.requestData.es_drfca = 1;
+        this.start('startFile', true);
+        this.requestData.es_drfca = 0;
+    }
+    else
+    {
+        var jmsgs = {info:[Joomla.JText._('COM_EASYSTAGING_JSON_TEST_RSYNC_PLAN_NOT_PUBLISHED')]};
+        Joomla.renderMessages(jmsgs);
+    }
 };
 
